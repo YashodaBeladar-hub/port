43b9ca5579492ac37cd47a3502d34e8584965ab6
Yashoda Beladar <yashodabeladar@gmail.com>
2024-10-17 22:14:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+        menuBtn.className += "responsive";
+    }else{
+        menuBtn.className = "nav-menu";
+    }
+}
+
+window.onscroll = function() {headerShadow()}
+
+function headerShadow(){
+    const navHeader = document.getElementById("header");
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        navHeader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight= "70px";
+    }else{
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight= "90px";
+    }
+}
+
+
+var typed = new Typed('.typedText', {
+    strings: ['Web Developer !', 'Web Designer !'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
